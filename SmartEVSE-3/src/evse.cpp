b7f6c79474de07bc8eac3f364eddf5fede7bbcaf
dingo35 <spaarder@hotmail.nl>
2024-03-16 15:10:44 -0400

    ssi.html, evse.cpp: add SSI, not working, commented

--- SmartEVSE-3/src/evse.cpp
+++ SmartEVSE-3/src/evse.cpp
@@ -4407,6 +4407,8 @@ static void fn(struct mg_connection *c, int ev, void *ev_data) {
     } else {                                                                    // if everything else fails, serve static page
         struct mg_fs fs = mg_fs_posix;
         fs.st = my_stat;
+        //in order to enable ssi, compile with -DMG_ENABLE_SSI=1
+        //struct mg_http_serve_opts opts = {.root_dir = FS_ROOT, .ssi_pattern = "/spiffs/index.html", .extra_headers = NULL, .mime_types = NULL, .page404 = NULL, .fs = &fs };
         struct mg_http_serve_opts opts = {.root_dir = FS_ROOT, .ssi_pattern = NULL, .extra_headers = NULL, .mime_types = NULL, .page404 = NULL, .fs = &fs };
         opts.fs = NULL;
         mg_http_serve_dir(c, hm, &opts);
